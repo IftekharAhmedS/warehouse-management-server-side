5171f8a16818d44591dfe1cd96907a9eb6c3a168
IftekharAhmedS <ahamedsantonu@gmail.com>
2022-05-04 23:10:16 +0600

    added comments

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var jwt = require('jsonwebtoken');
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 
+// Middlewares
+
 app.use(cors());
 app.use(express.json());
+
+// JWT Verification Function
 
 function verifyToken(req, res, next){
   const authHeader = req.headers.authorization;
@@ -24,7 +28,6 @@ function verifyToken(req, res, next){
   })
 }
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7pbou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -39,6 +42,8 @@ const run = async () => {
       res.send({ accessKey });
     })
 
+    // Item API
+
     app.get('/items', verifyToken , async (req, res) => {
       const email = req.query.email;
       const query = {};
@@ -46,18 +51,43 @@ const run = async () => {
       const item = await cursor.toArray();
       res.send(item)
     })
+
+    // Single Item API
+
     app.get('/items/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
       const item = await itemCollections.findOne(query);
       res.send(item)
     })
+
+    // Update Item API
+
+    app.put('/items/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const updateItem = req.body;
+      const filter = {_id: ObjectId(id)};
+      const options = {upsert: true}
+      const updateFields = {
+        $set: {
+          productQTY: updateItem.productQTY
+        }
+      }
+      const item = await itemCollections.updateOne(filter, updateFields, options);
+      res.send(item)
+    })
+
+    // Delete Item API
+
     app.delete('/items/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
       const item = await itemCollections.deleteOne(query);
       res.send(item)
     })
+
+    // Get Filtered Item API
+
     app.get('/filtered-items', verifyToken , async (req, res) => {
       const email = req.query.email;
       const decodedEmail = req.decoded.email;
@@ -72,14 +102,13 @@ const run = async () => {
       }
     })
 
+    // Add New Item API
+
     app.post('/items', async (req, res) => {
       const newItem = req.body;
       const item = await itemCollections.insertOne(newItem);
       res.send(item)
   })
-
-
-
 
     console.log('DB connected')
   }
